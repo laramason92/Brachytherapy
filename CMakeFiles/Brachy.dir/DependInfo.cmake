1057fd6467037d1ad03228d6ea2328effb077776
Lara Mason <lara.mason@live.com>
2018-05-10 12:50:51 +0200

    First commit

---
+++ CMakeFiles/Brachy.dir/DependInfo.cmake
@@ -0,0 +1,56 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/Users/laramason/Downloads/Geant4/geant4.10.03.p03/examples/advanced/brachytherapy/Brachy.cc" "/Users/laramason/Downloads/Geant4/build-brachy-proj/CMakeFiles/Brachy.dir/Brachy.cc.o"
+  "/Users/laramason/Downloads/Geant4/geant4.10.03.p03/examples/advanced/brachytherapy/src/BrachyActionInitialization.cc" "/Users/laramason/Downloads/Geant4/build-brachy-proj/CMakeFiles/Brachy.dir/src/BrachyActionInitialization.cc.o"
+  "/Users/laramason/Downloads/Geant4/geant4.10.03.p03/examples/advanced/brachytherapy/src/BrachyAnalysisManager.cc" "/Users/laramason/Downloads/Geant4/build-brachy-proj/CMakeFiles/Brachy.dir/src/BrachyAnalysisManager.cc.o"
+  "/Users/laramason/Downloads/Geant4/geant4.10.03.p03/examples/advanced/brachytherapy/src/BrachyDetectorConstruction.cc" "/Users/laramason/Downloads/Geant4/build-brachy-proj/CMakeFiles/Brachy.dir/src/BrachyDetectorConstruction.cc.o"
+  "/Users/laramason/Downloads/Geant4/geant4.10.03.p03/examples/advanced/brachytherapy/src/BrachyDetectorConstructionFlexi.cc" "/Users/laramason/Downloads/Geant4/build-brachy-proj/CMakeFiles/Brachy.dir/src/BrachyDetectorConstructionFlexi.cc.o"
+  "/Users/laramason/Downloads/Geant4/geant4.10.03.p03/examples/advanced/brachytherapy/src/BrachyDetectorConstructionGammaMed.cc" "/Users/laramason/Downloads/Geant4/build-brachy-proj/CMakeFiles/Brachy.dir/src/BrachyDetectorConstructionGammaMed.cc.o"
+  "/Users/laramason/Downloads/Geant4/geant4.10.03.p03/examples/advanced/brachytherapy/src/BrachyDetectorConstructionI.cc" "/Users/laramason/Downloads/Geant4/build-brachy-proj/CMakeFiles/Brachy.dir/src/BrachyDetectorConstructionI.cc.o"
+  "/Users/laramason/Downloads/Geant4/geant4.10.03.p03/examples/advanced/brachytherapy/src/BrachyDetectorConstructionLeipzig.cc" "/Users/laramason/Downloads/Geant4/build-brachy-proj/CMakeFiles/Brachy.dir/src/BrachyDetectorConstructionLeipzig.cc.o"
+  "/Users/laramason/Downloads/Geant4/geant4.10.03.p03/examples/advanced/brachytherapy/src/BrachyDetectorConstructionTG186.cc" "/Users/laramason/Downloads/Geant4/build-brachy-proj/CMakeFiles/Brachy.dir/src/BrachyDetectorConstructionTG186.cc.o"
+  "/Users/laramason/Downloads/Geant4/geant4.10.03.p03/examples/advanced/brachytherapy/src/BrachyDetectorMessenger.cc" "/Users/laramason/Downloads/Geant4/build-brachy-proj/CMakeFiles/Brachy.dir/src/BrachyDetectorMessenger.cc.o"
+  "/Users/laramason/Downloads/Geant4/geant4.10.03.p03/examples/advanced/brachytherapy/src/BrachyFactory.cc" "/Users/laramason/Downloads/Geant4/build-brachy-proj/CMakeFiles/Brachy.dir/src/BrachyFactory.cc.o"
+  "/Users/laramason/Downloads/Geant4/geant4.10.03.p03/examples/advanced/brachytherapy/src/BrachyFactoryFlexi.cc" "/Users/laramason/Downloads/Geant4/build-brachy-proj/CMakeFiles/Brachy.dir/src/BrachyFactoryFlexi.cc.o"
+  "/Users/laramason/Downloads/Geant4/geant4.10.03.p03/examples/advanced/brachytherapy/src/BrachyFactoryGammaMed.cc" "/Users/laramason/Downloads/Geant4/build-brachy-proj/CMakeFiles/Brachy.dir/src/BrachyFactoryGammaMed.cc.o"
+  "/Users/laramason/Downloads/Geant4/geant4.10.03.p03/examples/advanced/brachytherapy/src/BrachyFactoryI.cc" "/Users/laramason/Downloads/Geant4/build-brachy-proj/CMakeFiles/Brachy.dir/src/BrachyFactoryI.cc.o"
+  "/Users/laramason/Downloads/Geant4/geant4.10.03.p03/examples/advanced/brachytherapy/src/BrachyFactoryLeipzig.cc" "/Users/laramason/Downloads/Geant4/build-brachy-proj/CMakeFiles/Brachy.dir/src/BrachyFactoryLeipzig.cc.o"
+  "/Users/laramason/Downloads/Geant4/geant4.10.03.p03/examples/advanced/brachytherapy/src/BrachyFactoryTG186.cc" "/Users/laramason/Downloads/Geant4/build-brachy-proj/CMakeFiles/Brachy.dir/src/BrachyFactoryTG186.cc.o"
+  "/Users/laramason/Downloads/Geant4/geant4.10.03.p03/examples/advanced/brachytherapy/src/BrachyMaterial.cc" "/Users/laramason/Downloads/Geant4/build-brachy-proj/CMakeFiles/Brachy.dir/src/BrachyMaterial.cc.o"
+  "/Users/laramason/Downloads/Geant4/geant4.10.03.p03/examples/advanced/brachytherapy/src/BrachyPhysicsList.cc" "/Users/laramason/Downloads/Geant4/build-brachy-proj/CMakeFiles/Brachy.dir/src/BrachyPhysicsList.cc.o"
+  "/Users/laramason/Downloads/Geant4/geant4.10.03.p03/examples/advanced/brachytherapy/src/BrachyPrimaryGeneratorAction.cc" "/Users/laramason/Downloads/Geant4/build-brachy-proj/CMakeFiles/Brachy.dir/src/BrachyPrimaryGeneratorAction.cc.o"
+  "/Users/laramason/Downloads/Geant4/geant4.10.03.p03/examples/advanced/brachytherapy/src/BrachyRunAction.cc" "/Users/laramason/Downloads/Geant4/build-brachy-proj/CMakeFiles/Brachy.dir/src/BrachyRunAction.cc.o"
+  "/Users/laramason/Downloads/Geant4/geant4.10.03.p03/examples/advanced/brachytherapy/src/BrachySteppingAction.cc" "/Users/laramason/Downloads/Geant4/build-brachy-proj/CMakeFiles/Brachy.dir/src/BrachySteppingAction.cc.o"
+  "/Users/laramason/Downloads/Geant4/geant4.10.03.p03/examples/advanced/brachytherapy/src/BrachyUserScoreWriter.cc" "/Users/laramason/Downloads/Geant4/build-brachy-proj/CMakeFiles/Brachy.dir/src/BrachyUserScoreWriter.cc.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "Clang")
+
+# Preprocessor definitions for this target.
+set(CMAKE_TARGET_DEFINITIONS_CXX
+  "ANALYSIS_USE"
+  "G4INTY_USE_XT"
+  "G4UI_USE"
+  "G4UI_USE_TCSH"
+  "G4VERBOSE"
+  "G4VIS_USE"
+  "G4VIS_USE_OPENGL"
+  "G4VIS_USE_OPENGLX"
+  "G4_STORE_TRAJECTORY"
+  )
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "/Users/laramason/Downloads/Geant4/geant4.10.03.p03-install/include/Geant4"
+  "/Users/laramason/Downloads/Geant4/geant4.10.03.p03/examples/advanced/brachytherapy/include"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ CMakeFiles/Brachy.dir/cmake_clean.cmake
@@ -0,0 +1,31 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Brachy.dir/Brachy.cc.o"
+  "CMakeFiles/Brachy.dir/src/BrachyActionInitialization.cc.o"
+  "CMakeFiles/Brachy.dir/src/BrachyAnalysisManager.cc.o"
+  "CMakeFiles/Brachy.dir/src/BrachyDetectorConstruction.cc.o"
+  "CMakeFiles/Brachy.dir/src/BrachyDetectorConstructionFlexi.cc.o"
+  "CMakeFiles/Brachy.dir/src/BrachyDetectorConstructionGammaMed.cc.o"
+  "CMakeFiles/Brachy.dir/src/BrachyDetectorConstructionI.cc.o"
+  "CMakeFiles/Brachy.dir/src/BrachyDetectorConstructionLeipzig.cc.o"
+  "CMakeFiles/Brachy.dir/src/BrachyDetectorConstructionTG186.cc.o"
+  "CMakeFiles/Brachy.dir/src/BrachyDetectorMessenger.cc.o"
+  "CMakeFiles/Brachy.dir/src/BrachyFactory.cc.o"
+  "CMakeFiles/Brachy.dir/src/BrachyFactoryFlexi.cc.o"
+  "CMakeFiles/Brachy.dir/src/BrachyFactoryGammaMed.cc.o"
+  "CMakeFiles/Brachy.dir/src/BrachyFactoryI.cc.o"
+  "CMakeFiles/Brachy.dir/src/BrachyFactoryLeipzig.cc.o"
+  "CMakeFiles/Brachy.dir/src/BrachyFactoryTG186.cc.o"
+  "CMakeFiles/Brachy.dir/src/BrachyMaterial.cc.o"
+  "CMakeFiles/Brachy.dir/src/BrachyPhysicsList.cc.o"
+  "CMakeFiles/Brachy.dir/src/BrachyPrimaryGeneratorAction.cc.o"
+  "CMakeFiles/Brachy.dir/src/BrachyRunAction.cc.o"
+  "CMakeFiles/Brachy.dir/src/BrachySteppingAction.cc.o"
+  "CMakeFiles/Brachy.dir/src/BrachyUserScoreWriter.cc.o"
+  "Brachy.pdb"
+  "Brachy"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Brachy.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
