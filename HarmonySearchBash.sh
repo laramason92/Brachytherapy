9e061a1258c93a6095eaf77d128c2a113fa42577
Lara Mason <lara.mason@live.com>
2018-09-02 07:33:15 +0200

    fixed mem macro HS

--- HarmonySearchBash.sh
+++ HarmonySearchBash.sh
@@ -1,11 +1,6 @@
 #!/bin/bash
 
 echo "hello world"
-source /Users/laramason/Downloads/Geant4/geant4.10.03.p03-install/bin/geant4.sh
-source /Users/laramason//Downloads/ROOT/root-build/bin/thisroot.sh
-export G4ENSDFSTATEDATA=/Users/laramason/Downloads/Geant4/geant4.10.03.p03-install/share/Geant4-10.3.3/data/G4ENSDFSTATE2.1;export G4LEDATA=/Users/laramason/Downloads/Geant4/geant4.10.03.p03-install/share/Geant4-10.3.3/data/G4EMLOW6.50/;export G4LEVELGAMMADATA=/Users/laramason/Downloads/Geant4/geant4.10.03.p03-install/share/Geant4-10.3.3/data/PhotonEvaporation4.3.2/;export G4SAIDXSDATA=/Users/laramason/Downloads/Geant4/geant4.10.03.p03-install/share/Geant4-10.3.3/data/G4SAIDDATA1.1;export G4NEUTRONXSDATA=/Users/laramason/Downloads/Geant4/geant4.10.03.p03-install/share/Geant4-10.3.3/data/G4NEUTRONXS1.4
-
-/Users/laramason/Downloads/Geant4/build-brachy-proj/Brachy Macro_HM_all_100.mac 
 
 root Harmony_Memory_Construction.C
 echo "**********************************"
--- Harmony_New_Vector.C
+++ Harmony_New_Vector.C
@@ -75,7 +75,7 @@ data.push_back(new_candidate);
 
 for (int s=0; s!=data.size(); s++){
  for (int l=0; l!=data[0].size(); l++){
- //std::cout << data[s][l] << std::endl; //reads out the randomly selected vector in the HM.
+ std::cout << data[s][l] << std::endl; //reads out the randomly selected vector in the HM.
  }
 }
 
@@ -84,8 +84,400 @@ for (int s=0; s!=data.size(); s++){
 ofstream HarmonyMemory_New;
 HarmonyMemory_New.open("HarmonyMemory_New.txt");
 
+ofstream Macro_HM_1;
+ofstream Macro_HM_2;
+ofstream Macro_HM_3;
+ofstream Macro_HM_4;
+ofstream Macro_HM_5;
+
+Macro_HM_1.open("Macro_HM_1.mac");
+Macro_HM_2.open("Macro_HM_2.mac");
+Macro_HM_3.open("Macro_HM_3.mac");
+Macro_HM_4.open("Macro_HM_4.mac");
+Macro_HM_5.open("Macro_HM_5.mac");
+
 ofstream Macro_HM_new;
 Macro_HM_new.open("Macro_HM_new.mac");
+
+Macro_HM_1 << "/control/verbose 1" << "\n";
+Macro_HM_1 << "/tracking/verbose 0" << "\n";
+Macro_HM_1 << "/run/verbose 0" << "\n";
+Macro_HM_1 << "/event/verbose 0" << "\n";
+Macro_HM_1 << "/source/switch GammaMed" << "\n";
+Macro_HM_1 << "/control/execute iridium_source_primary.mac" << "\n";
+
+Macro_HM_1 << "/score/create/boxMesh boxMesh_4" << "\n";
+Macro_HM_1 << "/score/mesh/boxSize 0.5 0.0125 0.5 cm" << "\n";
+Macro_HM_1 << "/score/mesh/nBin 1 1 1" << "\n";
+Macro_HM_1 << "/score/quantity/doseDeposit doseDep" << "\n";
+Macro_HM_1 << "/score/mesh/translate/xyz -1 -3. 0. cm" << "\n";
+Macro_HM_1 << "/score/close" << "\n";
+
+Macro_HM_1 << "/score/create/boxMesh boxMesh_5" << "\n";
+Macro_HM_1 << "/score/mesh/boxSize 0.5 0.0125 0.5 cm" << "\n";
+Macro_HM_1 << "/score/mesh/nBin 1 1 1" << "\n";
+Macro_HM_1 << "/score/quantity/doseDeposit doseDep" << "\n";
+Macro_HM_1 << "/score/mesh/translate/xyz 0 2. 2.3 cm" << "\n";
+Macro_HM_1 << "/score/close" << "\n";
+
+Macro_HM_1 << "/score/list" << "\n";
+
+Macro_HM_2 << "/control/verbose 1" << "\n";
+Macro_HM_2 << "/tracking/verbose 0" << "\n";
+Macro_HM_2 << "/run/verbose 0" << "\n";
+Macro_HM_2 << "/event/verbose 0" << "\n";
+Macro_HM_2 << "/source/switch GammaMed" << "\n";
+Macro_HM_2 << "/control/execute iridium_source_primary.mac" << "\n";
+
+Macro_HM_2 << "/score/create/boxMesh boxMesh_4" << "\n";
+Macro_HM_2 << "/score/mesh/boxSize 0.5 0.0125 0.5 cm" << "\n";
+Macro_HM_2 << "/score/mesh/nBin 1 1 1" << "\n";
+Macro_HM_2 << "/score/quantity/doseDeposit doseDep" << "\n";
+Macro_HM_2 << "/score/mesh/translate/xyz -1 -3. 0. cm" << "\n";
+Macro_HM_2 << "/score/close" << "\n";
+
+Macro_HM_2 << "/score/create/boxMesh boxMesh_5" << "\n";
+Macro_HM_2 << "/score/mesh/boxSize 0.5 0.0125 0.5 cm" << "\n";
+Macro_HM_2 << "/score/mesh/nBin 1 1 1" << "\n";
+Macro_HM_2 << "/score/quantity/doseDeposit doseDep" << "\n";
+Macro_HM_2 << "/score/mesh/translate/xyz 0 2. 2.3 cm" << "\n";
+Macro_HM_2 << "/score/close" << "\n";
+
+Macro_HM_2 << "/score/list" << "\n";
+
+Macro_HM_3 << "/control/verbose 1" << "\n";
+Macro_HM_3 << "/tracking/verbose 0" << "\n";
+Macro_HM_3 << "/run/verbose 0" << "\n";
+Macro_HM_3 << "/event/verbose 0" << "\n";
+Macro_HM_3 << "/source/switch GammaMed" << "\n";
+Macro_HM_3 << "/control/execute iridium_source_primary.mac" << "\n";
+
+Macro_HM_3 << "/score/create/boxMesh boxMesh_4" << "\n";
+Macro_HM_3 << "/score/mesh/boxSize 0.5 0.0125 0.5 cm" << "\n";
+Macro_HM_3 << "/score/mesh/nBin 1 1 1" << "\n";
+Macro_HM_3 << "/score/quantity/doseDeposit doseDep" << "\n";
+Macro_HM_3 << "/score/mesh/translate/xyz -1 -3. 0. cm" << "\n";
+Macro_HM_3 << "/score/close" << "\n";
+
+Macro_HM_3 << "/score/create/boxMesh boxMesh_5" << "\n";
+Macro_HM_3 << "/score/mesh/boxSize 0.5 0.0125 0.5 cm" << "\n";
+Macro_HM_3 << "/score/mesh/nBin 1 1 1" << "\n";
+Macro_HM_3 << "/score/quantity/doseDeposit doseDep" << "\n";
+Macro_HM_3 << "/score/mesh/translate/xyz 0 2. 2.3 cm" << "\n";
+Macro_HM_3 << "/score/close" << "\n";
+
+Macro_HM_3 << "/score/list" << "\n";
+
+Macro_HM_4 << "/control/verbose 1" << "\n";
+Macro_HM_4 << "/tracking/verbose 0" << "\n";
+Macro_HM_4 << "/run/verbose 0" << "\n";
+Macro_HM_4 << "/event/verbose 0" << "\n";
+Macro_HM_4 << "/source/switch GammaMed" << "\n";
+Macro_HM_4 << "/control/execute iridium_source_primary.mac" << "\n";
+
+Macro_HM_4 << "/score/create/boxMesh boxMesh_4" << "\n";
+Macro_HM_4 << "/score/mesh/boxSize 0.5 0.0125 0.5 cm" << "\n";
+Macro_HM_4 << "/score/mesh/nBin 1 1 1" << "\n";
+Macro_HM_4 << "/score/quantity/doseDeposit doseDep" << "\n";
+Macro_HM_4 << "/score/mesh/translate/xyz -1 -3. 0. cm" << "\n";
+Macro_HM_4 << "/score/close" << "\n";
+
+Macro_HM_4 << "/score/create/boxMesh boxMesh_5" << "\n";
+Macro_HM_4 << "/score/mesh/boxSize 0.5 0.0125 0.5 cm" << "\n";
+Macro_HM_4 << "/score/mesh/nBin 1 1 1" << "\n";
+Macro_HM_4 << "/score/quantity/doseDeposit doseDep" << "\n";
+Macro_HM_4 << "/score/mesh/translate/xyz 0 2. 2.3 cm" << "\n";
+Macro_HM_4 << "/score/close" << "\n";
+
+Macro_HM_4 << "/score/list" << "\n";
+
+Macro_HM_5 << "/control/verbose 1" << "\n";
+Macro_HM_5 << "/tracking/verbose 0" << "\n";
+Macro_HM_5 << "/run/verbose 0" << "\n";
+Macro_HM_5 << "/event/verbose 0" << "\n";
+Macro_HM_5 << "/source/switch GammaMed" << "\n";
+Macro_HM_5 << "/control/execute iridium_source_primary.mac" << "\n";
+
+Macro_HM_5 << "/score/create/boxMesh boxMesh_4" << "\n";
+Macro_HM_5 << "/score/mesh/boxSize 0.5 0.0125 0.5 cm" << "\n";
+Macro_HM_5 << "/score/mesh/nBin 1 1 1" << "\n";
+Macro_HM_5 << "/score/quantity/doseDeposit doseDep" << "\n";
+Macro_HM_5 << "/score/mesh/translate/xyz -1 -3. 0. cm" << "\n";
+Macro_HM_5 << "/score/close" << "\n";
+
+Macro_HM_5 << "/score/create/boxMesh boxMesh_5" << "\n";
+Macro_HM_5 << "/score/mesh/boxSize 0.5 0.0125 0.5 cm" << "\n";
+Macro_HM_5 << "/score/mesh/nBin 1 1 1" << "\n";
+Macro_HM_5 << "/score/quantity/doseDeposit doseDep" << "\n";
+Macro_HM_5 << "/score/mesh/translate/xyz 0 2. 2.3 cm" << "\n";
+Macro_HM_5 << "/score/close" << "\n";
+
+Macro_HM_5 << "/score/list" << "\n";
+
+for (int s=0; s<HMS; s++)
+ {
+  for (int m=0; m<vec_length; m++)
+  {
+  if (s==0){
+   if (m==0){ //if 0 no need to move source
+   Macro_HM_1 << "/gammamed/detector/SourceTranslationZ 1." <<"\n";
+   Macro_HM_1 << "/control/execute source_HM_1.mac"  <<"\n";
+   }
+   if (m==1){
+   Macro_HM_1 << "/gammamed/detector/SourceTranslationZ 2." <<"\n";
+   Macro_HM_1 << "/control/execute source_HM_2.mac"  <<"\n";
+   }
+   if (m==2){
+   Macro_HM_1 << "/gammamed/detector/SourceTranslationZ 3." <<"\n";
+   Macro_HM_1 << "/control/execute source_HM_3.mac"  <<"\n";
+   }
+   if (m==3){
+   Macro_HM_1 << "/gammamed/detector/SourceTranslationZ 4." <<"\n";
+   Macro_HM_1 << "/control/execute source_HM_4.mac"  <<"\n";
+   }
+   if (m==4){
+   Macro_HM_1 << "/gammamed/detector/SourceTranslationZ 5." <<"\n";
+   Macro_HM_1 << "/control/execute source_HM_5.mac"  <<"\n";
+   }
+   if (m==5){
+   Macro_HM_1 << "/gammamed/detector/SourceTranslationZ 6." <<"\n";
+   Macro_HM_1 << "/control/execute source_HM_6.mac"  <<"\n";
+   }
+   if (m==6){
+   Macro_HM_1 << "/gammamed/detector/SourceTranslationZ 7." <<"\n";
+   Macro_HM_1 << "/control/execute source_HM_7.mac"  <<"\n";
+   }
+   if (m==7){
+   Macro_HM_1 << "/gammamed/detector/SourceTranslationZ 8." <<"\n";
+   Macro_HM_1 << "/control/execute source_HM_8.mac"  <<"\n";
+   }
+   if (m==8){
+   Macro_HM_1 << "/gammamed/detector/SourceTranslationZ 9." <<"\n";
+   Macro_HM_1 << "/control/execute source_HM_9.mac"  <<"\n";
+   }
+   if (m==9){
+   Macro_HM_1 << "/gammamed/detector/SourceTranslationZ 10." <<"\n";
+   Macro_HM_1 << "/control/execute source_HM_10.mac"  <<"\n";
+   }
+   if (m==10){
+   Macro_HM_1 << "/gammamed/detector/SourceTranslationZ 11." <<"\n";
+   Macro_HM_1 << "/control/execute source_HM_11.mac"  <<"\n";
+   }
+   Macro_HM_1 << "/run/beamOn " << data[s][m] << "\n"; //HOW TO TELL THE CODE TO PRINT THE CORRECT SOURCE POSITION MACRO HERE 
+   }
+// *****************************************************************************//
+  if (s==1){
+   if (m==0){ //if 0 no need to move source
+   Macro_HM_2 << "/gammamed/detector/SourceTranslationZ 1." <<"\n";
+   Macro_HM_2 << "/control/execute source_HM_1.mac"  <<"\n";
+   }
+   if (m==1){
+   Macro_HM_2 << "/gammamed/detector/SourceTranslationZ 2." <<"\n";
+   Macro_HM_2 << "/control/execute source_HM_2.mac"  <<"\n";
+   }
+   if (m==2){
+   Macro_HM_2 << "/gammamed/detector/SourceTranslationZ 3." <<"\n";
+   Macro_HM_2 << "/control/execute source_HM_3.mac"  <<"\n";
+   }
+   if (m==3){
+   Macro_HM_2 << "/gammamed/detector/SourceTranslationZ 4." <<"\n";
+   Macro_HM_2 << "/control/execute source_HM_4.mac"  <<"\n";
+   }
+   if (m==4){
+   Macro_HM_2 << "/gammamed/detector/SourceTranslationZ 5." <<"\n";
+   Macro_HM_2 << "/control/execute source_HM_5.mac"  <<"\n";
+   }
+   if (m==5){
+   Macro_HM_2 << "/gammamed/detector/SourceTranslationZ 6." <<"\n";
+   Macro_HM_2 << "/control/execute source_HM_6.mac"  <<"\n";
+   }
+   if (m==6){
+   Macro_HM_2 << "/gammamed/detector/SourceTranslationZ 7." <<"\n";
+   Macro_HM_2 << "/control/execute source_HM_7.mac"  <<"\n";
+   }
+   if (m==7){
+   Macro_HM_2 << "/gammamed/detector/SourceTranslationZ 8." <<"\n";
+   Macro_HM_2 << "/control/execute source_HM_8.mac"  <<"\n";
+   }
+   if (m==8){
+   Macro_HM_2 << "/gammamed/detector/SourceTranslationZ 9." <<"\n";
+   Macro_HM_2 << "/control/execute source_HM_9.mac"  <<"\n";
+   }
+   if (m==9){
+   Macro_HM_2 << "/gammamed/detector/SourceTranslationZ 10." <<"\n";
+   Macro_HM_2 << "/control/execute source_HM_10.mac"  <<"\n";
+   }
+   if (m==10){
+   Macro_HM_2 << "/gammamed/detector/SourceTranslationZ 11." <<"\n";
+   Macro_HM_2 << "/control/execute source_HM_11.mac"  <<"\n";
+   }
+   Macro_HM_2 << "/run/beamOn " << data[s][m] << "\n";
+   }
+// *****************************************************************************//
+  if (s==2){
+   if (m==0){ //if 0 no need to move source
+   Macro_HM_3 << "/gammamed/detector/SourceTranslationZ 1." <<"\n";
+   Macro_HM_3 << "/control/execute source_HM_1.mac"  <<"\n";
+   }
+   if (m==1){
+   Macro_HM_3 << "/gammamed/detector/SourceTranslationZ 2." <<"\n";
+   Macro_HM_3 << "/control/execute source_HM_2.mac"  <<"\n";
+   }
+   if (m==2){
+   Macro_HM_3 << "/gammamed/detector/SourceTranslationZ 3." <<"\n";
+   Macro_HM_3 << "/control/execute source_HM_3.mac"  <<"\n";
+   }
+   if (m==3){
+   Macro_HM_3 << "/gammamed/detector/SourceTranslationZ 4." <<"\n";
+   Macro_HM_3 << "/control/execute source_HM_4.mac"  <<"\n";
+   }
+   if (m==4){
+   Macro_HM_3 << "/gammamed/detector/SourceTranslationZ 5." <<"\n";
+   Macro_HM_3 << "/control/execute source_HM_5.mac"  <<"\n";
+   }
+   if (m==5){
+   Macro_HM_3 << "/gammamed/detector/SourceTranslationZ 6." <<"\n";
+   Macro_HM_3 << "/control/execute source_HM_6.mac"  <<"\n";
+   }
+   if (m==6){
+   Macro_HM_3 << "/gammamed/detector/SourceTranslationZ 7." <<"\n";
+   Macro_HM_3 << "/control/execute source_HM_7.mac"  <<"\n";
+   }
+   if (m==7){
+   Macro_HM_3 << "/gammamed/detector/SourceTranslationZ 8." <<"\n";
+   Macro_HM_3 << "/control/execute source_HM_8.mac"  <<"\n";
+   }
+   if (m==8){
+   Macro_HM_3 << "/gammamed/detector/SourceTranslationZ 9." <<"\n";
+   Macro_HM_3 << "/control/execute source_HM_9.mac"  <<"\n";
+   }
+   if (m==9){
+   Macro_HM_3 << "/gammamed/detector/SourceTranslationZ 10." <<"\n";
+   Macro_HM_3 << "/control/execute source_HM_10.mac"  <<"\n";
+   }
+   if (m==10){
+   Macro_HM_3 << "/gammamed/detector/SourceTranslationZ 11." <<"\n";
+   Macro_HM_3 << "/control/execute source_HM_11.mac"  <<"\n";
+   }
+   Macro_HM_3 << "/run/beamOn " << data[s][m] << "\n";
+   }
+// *****************************************************************************//
+  if (s==3){
+   if (m==0){ //if 0 no need to move source
+   Macro_HM_4 << "/gammamed/detector/SourceTranslationZ 1." <<"\n";
+   Macro_HM_4 << "/control/execute source_HM_1.mac"  <<"\n";
+   }
+   if (m==1){
+   Macro_HM_4 << "/gammamed/detector/SourceTranslationZ 2." <<"\n";
+   Macro_HM_4 << "/control/execute source_HM_2.mac"  <<"\n";
+   }
+   if (m==2){
+   Macro_HM_4 << "/gammamed/detector/SourceTranslationZ 3." <<"\n";
+   Macro_HM_4 << "/control/execute source_HM_3.mac"  <<"\n";
+   }
+   if (m==3){
+   Macro_HM_4 << "/gammamed/detector/SourceTranslationZ 4." <<"\n";
+   Macro_HM_4 << "/control/execute source_HM_4.mac"  <<"\n";
+   }
+   if (m==4){
+   Macro_HM_4 << "/gammamed/detector/SourceTranslationZ 5." <<"\n";
+   Macro_HM_4 << "/control/execute source_HM_5.mac"  <<"\n";
+   }
+   if (m==5){
+   Macro_HM_4 << "/gammamed/detector/SourceTranslationZ 6." <<"\n";
+   Macro_HM_4 << "/control/execute source_HM_6.mac"  <<"\n";
+   }
+   if (m==6){
+   Macro_HM_4 << "/gammamed/detector/SourceTranslationZ 7." <<"\n";
+   Macro_HM_4 << "/control/execute source_HM_7.mac"  <<"\n";
+   }
+   if (m==7){
+   Macro_HM_4 << "/gammamed/detector/SourceTranslationZ 8." <<"\n";
+   Macro_HM_4 << "/control/execute source_HM_8.mac"  <<"\n";
+   }
+   if (m==8){
+   Macro_HM_4 << "/gammamed/detector/SourceTranslationZ 9." <<"\n";
+   Macro_HM_4 << "/control/execute source_HM_9.mac"  <<"\n";
+   }
+   if (m==9){
+   Macro_HM_4 << "/gammamed/detector/SourceTranslationZ 10." <<"\n";
+   Macro_HM_4 << "/control/execute source_HM_10.mac"  <<"\n";
+   }
+   if (m==10){
+   Macro_HM_4 << "/gammamed/detector/SourceTranslationZ 11." <<"\n";
+   Macro_HM_4 << "/control/execute source_HM_11.mac"  <<"\n";
+   }
+   Macro_HM_4 << "/run/beamOn " << data[s][m]*secs_to_events << "\n";
+   }
+// *****************************************************************************//
+  if (s==4){
+   if (m==0){ //if 0 no need to move source
+   Macro_HM_5 << "/gammamed/detector/SourceTranslationZ 1." <<"\n";
+   Macro_HM_5 << "/control/execute source_HM_1.mac"  <<"\n";
+   }
+   if (m==1){
+   Macro_HM_5 << "/gammamed/detector/SourceTranslationZ 2." <<"\n";
+   Macro_HM_5 << "/control/execute source_HM_2.mac"  <<"\n";
+   }
+   if (m==2){
+   Macro_HM_5 << "/gammamed/detector/SourceTranslationZ 3." <<"\n";
+   Macro_HM_5 << "/control/execute source_HM_3.mac"  <<"\n";
+   }
+   if (m==3){
+   Macro_HM_5 << "/gammamed/detector/SourceTranslationZ 4." <<"\n";
+   Macro_HM_5 << "/control/execute source_HM_4.mac"  <<"\n";
+   }
+   if (m==4){
+   Macro_HM_5 << "/gammamed/detector/SourceTranslationZ 5." <<"\n";
+   Macro_HM_5 << "/control/execute source_HM_5.mac"  <<"\n";
+   }
+   if (m==5){
+   Macro_HM_5 << "/gammamed/detector/SourceTranslationZ 6." <<"\n";
+   Macro_HM_5 << "/control/execute source_HM_6.mac"  <<"\n";
+   }
+   if (m==6){
+   Macro_HM_5 << "/gammamed/detector/SourceTranslationZ 7." <<"\n";
+   Macro_HM_5 << "/control/execute source_HM_7.mac"  <<"\n";
+   }
+   if (m==7){
+   Macro_HM_5 << "/gammamed/detector/SourceTranslationZ 8." <<"\n";
+   Macro_HM_5 << "/control/execute source_HM_8.mac"  <<"\n";
+   }
+   if (m==8){
+   Macro_HM_5 << "/gammamed/detector/SourceTranslationZ 9." <<"\n";
+   Macro_HM_5 << "/control/execute source_HM_9.mac"  <<"\n";
+   }
+   if (m==9){
+   Macro_HM_5 << "/gammamed/detector/SourceTranslationZ 10." <<"\n";
+   Macro_HM_5 << "/control/execute source_HM_10.mac"  <<"\n";
+   }
+   if (m==10){
+   Macro_HM_5 << "/gammamed/detector/SourceTranslationZ 11." <<"\n";
+   Macro_HM_5 << "/control/execute source_HM_11.mac"  <<"\n";
+   }
+   Macro_HM_5 << "/run/beamOn " << data[s][m] << "\n";
+   }
+// *****************************************************************************//
+  }
+}
+
+Macro_HM_1 << "/score/dumpQuantityToFile boxMesh_5 doseDep EnergyDeposition_HM_1_ptA.out" << "\n";
+Macro_HM_2 << "/score/dumpQuantityToFile boxMesh_5 doseDep EnergyDeposition_HM_2_ptA.out" << "\n";
+Macro_HM_3 << "/score/dumpQuantityToFile boxMesh_5 doseDep EnergyDeposition_HM_3_ptA.out" << "\n";
+Macro_HM_4 << "/score/dumpQuantityToFile boxMesh_5 doseDep EnergyDeposition_HM_4_ptA.out" << "\n";
+Macro_HM_5 << "/score/dumpQuantityToFile boxMesh_5 doseDep EnergyDeposition_HM_5_ptA.out" << "\n";
+
+Macro_HM_1 << "/score/dumpQuantityToFile boxMesh_4 doseDep EnergyDeposition_HM_1_OAR.out" << "\n";
+Macro_HM_2 << "/score/dumpQuantityToFile boxMesh_4 doseDep EnergyDeposition_HM_2_OAR.out" << "\n";
+Macro_HM_3 << "/score/dumpQuantityToFile boxMesh_4 doseDep EnergyDeposition_HM_3_OAR.out" << "\n";
+Macro_HM_4 << "/score/dumpQuantityToFile boxMesh_4 doseDep EnergyDeposition_HM_4_OAR.out" << "\n";
+Macro_HM_5 << "/score/dumpQuantityToFile boxMesh_4 doseDep EnergyDeposition_HM_5_OAR.out" << "\n";
+
+Macro_HM_1.close();
+Macro_HM_2.close();
+Macro_HM_3.close();
+Macro_HM_4.close();
+Macro_HM_5.close();
 
 Macro_HM_new << "/control/verbose 1" << "\n";
 Macro_HM_new << "/tracking/verbose 0" << "\n";
